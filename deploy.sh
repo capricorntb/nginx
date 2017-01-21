85a0df3db32ceb2e6b0d5dd4ba2e7951fa672cc6
ida <yintb@126.com>
2017-01-21 10:14:52 +0800

    test

--- deploy.sh
+++ deploy.sh
@@ -6,6 +6,7 @@ ssh $server "source /etc/profile;
        	docker stop idatx ;
        	docker rm idatx ;
        	docker run -d --name idatx -p 80:80 -v /home/gitlab-runner/www:/var/www/html registry.cn-hangzhou.aliyuncs.com/boxfish/nginx:latest ;
+	scp www/index.html $server:/home/gitlab-runner/www
        	cd /home/gitlab-runner/www ;
 	cp index.html index.html.$datename;
 	sed -i 's/boxfish/fish/g' index.html"
